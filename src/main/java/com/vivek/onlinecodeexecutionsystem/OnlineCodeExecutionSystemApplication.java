21e29e56e9c9d475f36a11463ba3266918d97986
Vivek <vishwakarmavivek83@gmail.com>
2024-02-13 23:13:08 +0530

    Initial Commit

---
+++ src/main/java/com/vivek/onlinecodeexecutionsystem/OnlineCodeExecutionSystemApplication.java
@@ -0,0 +1,13 @@
+package com.vivek.onlinecodeexecutionsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineCodeExecutionSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineCodeExecutionSystemApplication.class, args);
+	}
+
+}
