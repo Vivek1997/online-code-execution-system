49fe200567be16a966f7379dcef0013deee5324a
Vivek <vishwakarmavivek83@gmail.com>
2024-03-07 01:53:08 +0530

    Feature: Added dockerfile for server and worker

--- worker/src/main/java/com/vivek/onlinecodeexecutionsystem/Worker.java
+++ worker/src/main/java/com/vivek/onlinecodeexecutionsystem/Worker.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Worker {
-
     public static void main(String[] args) {
         SpringApplication.run(Worker.class, args);
     }
-
 }
