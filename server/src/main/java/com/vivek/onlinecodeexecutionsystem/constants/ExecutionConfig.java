9f9e98b317ea1c2e6945a5c140e282b92c060f84
Vivek <vishwakarmavivek83@gmail.com>
2024-03-08 00:41:09 +0530

    Feature: Added documentation to controllers

--- server/src/main/java/com/vivek/onlinecodeexecutionsystem/constants/ExecutionConfig.java
+++ server/src/main/java/com/vivek/onlinecodeexecutionsystem/constants/ExecutionConfig.java
@@ -2,7 +2,7 @@ package com.vivek.onlinecodeexecutionsystem.constants;
 
 public class ExecutionConfig {
     public static final float MAX_CPU_TIME_LIMIT = 15;
-    public static final float MAX_CPU_EXTRA_TIME_LIMIT = 7;
+    public static final float MAX_CPU_EXTRA_TIME_LIMIT = 20;
     public static final float MAX_WALL_TIME_LIMIT = 30;
     public static final int MAX_STACK_LIMIT = 128000;
     public static final int MAX_MAX_PROCESSES_AND_OR_THREADS = 120;
